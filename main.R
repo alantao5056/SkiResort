843318b5db2cf1de55d90206759174f5bc592ff3
alantao5056 <alantaolr@outlook.com>
2023-11-26 10:41:16 -0500

    more plots

--- main.R
+++ main.R
@@ -1,4 +1,9 @@
 library(tidyverse)
+library(janitor)
+library(scales)
+library(rstanarm)
+library(gtsummary)
+library(ggpmisc)
 
 data <- read_csv("Ski-Resort-Dataset.csv")
 
@@ -23,3 +28,48 @@ data |>
   geom_smooth(se=FALSE) +
   scale_y_continuous(labels = scales::dollar_format()) +
   labs(title="Profits of Different Ski Resorts")
+
+temp = data |>
+  filter(Resort == "Alpine Arena") |>
+  lm(formula = Year ~ Profit)
+
+summary(temp)
+
+data |>
+  filter(Resort == "Mountain Meadows") |>
+  ggplot(aes(x = Year, y = Profit)) +
+  geom_point() +
+  geom_smooth(method = lm, se=FALSE) +
+  scale_y_continuous(labels = scales::dollar_format()) +
+  labs(title="Projection of Mountain Meadows") +
+  theme_bw()
+
+data |>
+  filter(Resort == "Alpine Arena") |>
+  ggplot(aes(x = Year, y = Profit)) +
+  geom_point() +
+  geom_smooth(method = lm, se=FALSE) +
+  scale_y_continuous(labels = scales::dollar_format()) +
+  labs(title="Projection of Alpine Arena") +
+  theme_bw()
+
+data |>
+  filter(Resort == "White Haven") |>
+  ggplot(aes(x = Year, y = Profit)) +
+  geom_point() +
+  geom_smooth(method = lm, se=FALSE) +
+  scale_y_continuous(labels = scales::dollar_format()) +
+  labs(title="Projection of White Haven") +
+  theme_bw()
+
+mm = data |>
+  filter(Resort == "Mountain Meadows")
+
+fitted = stan_glm(formula = Profit ~ Year,
+                    data = mm,
+                    refresh = 0,
+                    seed = 9)
+newobs = tibble(year=2050)
+
+temp = posterior_epred(fitted) |>
+  as_tibble()
